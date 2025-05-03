871d2c00ca7dd527cd2dcfd1eba38fae3eef6f9f
SumWasNotFound <sumeet0w0@gmail.com>
2025-05-03 12:53:51 +0000

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -290,5 +290,10 @@ const default_configuration = {
 };
 const CONFIG = new Config(default_configuration, palette);
 
+const root = document.querySelector(":root");
+root.style.setProperty("--bg", palette.mantle);
+root.style.setProperty("--accent", palette.green);
+
+
 
 
